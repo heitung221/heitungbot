2c6490b284b4829a4585d9646652ea44d4c55897
Heitung Lau <30768586+heitung221@users.noreply.github.com>
2018-07-31 16:10:24 +0800

    add calc mathjs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const mathjs = require('mathjs');
 
 var prefix = ",,";
 
@@ -210,7 +211,6 @@ bot.on("message", function(message){
 	}
 	console.log("Message: " + message.content);
 });
-
 
 
 
