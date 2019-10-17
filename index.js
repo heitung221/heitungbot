727428c9058d38dc1f34cdfe0d5f857433e672a6
Heitung Lau <30768586+heitung221@users.noreply.github.com>
2019-10-17 22:41:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -45,10 +45,10 @@ bot.on("message", function(message){
 		console.log(formattedTime);
 		//早起嘅雀仔有蟲食
 		
-		var jScheduleMessage = schedule.scheduleJob('53 17 * * *', function(){
+		var jScheduleMessage = schedule.scheduleJob('44 22 * * *', function(){
 			bot.channels.get("464090973807443968").send("scheduled Message 1753");
 		});
-		var jScheduleMessage2 = schedule.scheduleJob('53 09 * * *', function(){
+		var jScheduleMessage2 = schedule.scheduleJob('22 14 * * *', function(){
 			bot.channels.get("464090973807443968").send("scheduled Message 0953");
 		});
 		/*
